95944c99cdf0594870af94186ec36098cbdbf4be
Edward Hambraeus <edward.hambraeus@gmail.com>
2015-07-02 11:14:24 +0200

    Added map to locationtracker, with a marker

---
+++ FlagCap/app/src/main/java/com/sammy/edward/flagcap/MapsActivity.java
@@ -0,0 +1,29 @@
+package com.sammy.edward.flagcap;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.location_map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng lingon32 = new LatLng(-59.4633094, 17.9470539);
+        map.addMarker(new MarkerOptions().position(lingon32).title("Marker at lingonv. 32"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(lingon32));
+        map.moveCamera(CameraUpdateFactory.zoomTo(10));
+    }
+}
